ef6af226f3dc9a2b966b5a85eb52fd06576e4693
wangzenan <1024245303@qq.com>
2019-01-23 11:08:38 +0800

    修改seed

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -9,7 +9,7 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        $data = ['PHP', '前端', 'LINUX', '其他', '胡言乱语'];
+        $data = ['PHP', '前端', 'LINUX', '其他', '胡言乱语', 'MYSQL', '数学', '机器学习'];
         foreach ($data as $value){
             factory('App\Models\Category',1)->create([
                 'name' => $value,
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic tags example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
